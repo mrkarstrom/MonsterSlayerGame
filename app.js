8c3c283aec6f5c6de0e83eee40b5ee349e47b5e6
Magnus Karström <97023772+yeswebdotse@users.noreply.github.com>
2022-08-01 14:00:39 +0200

    Added New Game Button after Game is over

--- app.js
+++ app.js
@@ -12,9 +12,17 @@ const app = Vue.createApp({
   },
   computed: {
     monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        this.monsterHealth = 0;
+        return { width: '0%' };
+      }
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyles() {
+      if (this.playerHealth < 0) {
+        this.playerHealth = 0;
+        return { width: '0%' };
+      }
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -42,6 +50,12 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startNewGame() {
+      (this.monsterHealth = 100),
+        (this.playerHealth = 100),
+        (this.currentRound = 0),
+        (this.winner = null);
+    },
     attackMonster() {
       this.currentRound++;
       const strikeValue = getRandomValue(5, 12);
